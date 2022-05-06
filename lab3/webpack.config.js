2cd3ca8fc515590ebb701351b0ce9fb7cc61c353
Maksym Lisniak <maxim.l.forstudy@gmail.com>
2022-05-06 12:50:05 +0300

    lab5

--- lab3/webpack.config.js
+++ lab3/webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // installed via npm
 const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: 'development',
@@ -11,14 +12,41 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
+        filename: 'bundle.js',
     },
     plugins: [
-        new CleanWebpackPlugin(),
-        new CopyPlugin({
-            patterns: [
-              { from: "src", to: "" },
-            ]
-        })
+        // new CleanWebpackPlugin(),
+        // new CopyPlugin({
+        //     patterns: [
+        //       { from: "src/assets", to: "/assets" },
+        //     ]
+        // }),
+        new MiniCssExtractPlugin(),
+        // new HtmlWebpackPlugin({  // Also generate a test.html
+        //     filename: 'index.html',
+        //     template: 'src/pages/index.html'
+        //   })
     ],
+    // resolve: {
+    //     extensions: ['*', '.js', '.jsx'],
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ]
+    }
 }
